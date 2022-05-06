d27640d178afca5f44c2fd4e0ee5cdd1047843cc
Chad Rousseau <chad.o.rousseau@gmail.com>
2022-05-06 19:42:10 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Chad Rousseau ";
+    int age = 33 ;
+    String state = "Pennsylvania ";
+    System.out.print("Name: " +name);
+    System.out.printf("Age: " +age);
+    System.out.println(" State: " +state );
+  }
+}

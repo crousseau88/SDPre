5d665fd25a600bd3ab3ddecd892bfc0999efef47
Chad Rousseau <chad.o.rousseau@gmail.com>
2022-05-20 11:11:08 -0400

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,13 @@
+public class Objective3Lab3{
+  public static void main(String [] args){
+    int first, second, third, sum;
+
+      first = 1 + (int)(Math.random() * 6);
+      second = 1 + (int)(Math.random() * 6);
+      third = 1 + (int)(Math.random() * 6);
+       sum = (first + second + third);
+     System.out.println(+first+" + "+second+" + "+third+" = " +sum);
+//This simulates the rolling of a dice 3 times adding each roll to give the sum of all 3 rolls //
+
+  }
+}
